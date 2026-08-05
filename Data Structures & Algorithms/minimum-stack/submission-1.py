5dbffcb7b372954aef663f0ff0c2e896a066783f
Averie Ahn <averieahn@gmail.com>
2026-08-05 14:13:36 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,25 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack=[]
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+       
+
+    def pop(self) -> None:
+        self.stack[:]=self.stack[:-1]
+        
+
+    def top(self) -> int:
+        
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        min = self.stack[0]
+        for i in self.stack:
+            if i<min:
+                min = i
+        
+        return min
+        
